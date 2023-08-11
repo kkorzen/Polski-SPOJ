5eba874f361504152f329e44e1fe234205adbfc3
kkorzen <141860845+kkorzen@users.noreply.github.com>
2023-08-11 11:20:26 +0200

    Create ProblemCollatza-iteracyjnie.c

---
+++ Latwe/ProblemCollatza-iteracyjnie.c
@@ -0,0 +1,35 @@
+#define _CRT_SECURE_NO_WARNINGS
+ 
+#include <stdio.h>
+ 
+int Collatz(int n) {
+	int cnt = 0;
+	int x = n;
+ 
+	while (x != 1) {
+		if (x % 2 == 0) {
+			x = x/2;
+		}
+		else {
+			x = 3*x+1;
+		}
+		cnt++;
+	}
+ 
+	return cnt;
+ 
+}
+ 
+int main() {
+	int n;
+ 
+	int t = 0;
+	scanf("%d", &t);
+ 
+	for (int i = 0;i < t;i++) {
+		scanf("%d", &n);
+		printf("%d\n\n", Collatz(n));
+	}
+ 
+	return 0;
+} 
