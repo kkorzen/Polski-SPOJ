77ec1354dfa03ae62418471a11ef42b2f1a368b1
kkorzen <141860845+kkorzen@users.noreply.github.com>
2023-08-11 11:02:16 +0200

    Create PolePewnegoKola.c

---
+++ Latwe/PolePewnegoKola.c
@@ -0,0 +1,28 @@
+#define _CRT_SECURE_NO_WARNINGS
+ 
+#define PI 3.141592654
+ 
+#include <stdio.h>
+#include <math.h>
+ 
+double Calc_Area(double  r, double d) {
+	if (d == 0)
+		return PI * r * r;
+	if (d == 2 * r)
+		return 0.0f;
+ 
+	double x = sqrt(r * r - d*d/4);
+ 
+	return x * x * PI;
+ 
+}
+ 
+int main() {
+ 
+	double r, d;
+	scanf("%lf %lf", &r, &d);
+ 
+	printf("%lf", Calc_Area(r, d));
+ 
+	return 0;
+}
