755c9080d7197bc26814fd48aa7e06f453c8672a
kkorzen <141860845+kkorzen@users.noreply.github.com>
2023-08-11 11:29:56 +0200

    Create Kalkulator2.c

---
+++ Latwe/Kalkulator2.c
@@ -0,0 +1,42 @@
+#define _CRT_SECURE_NO_WARNINGS
+ 
+#include <stdio.h>
+#include <ctype.h>
+ 
+void Calc(char op, int a, int b, int reg[]) {
+	switch (op) {
+	case '+':
+		printf("%d\n", reg[a] + reg[b]);
+		break;
+	case '-':
+		printf("%d\n", reg[a] - reg[b]);
+		break;
+	case '*':
+		printf("%d\n", reg[a] * reg[b]);
+		break;
+	case '/':
+		printf("%d\n", reg[a] / reg[b]);
+		break;
+	case '%':
+		printf("%d\n", reg[a] % reg[b]);
+		break;
+	case 'z':
+		reg[a] = b;
+		break;
+	default:
+		break;
+	}
+}
+ 
+ 
+int main() {
+	char op;
+	int a, b;
+	int reg[10];
+	
+	while (scanf(" %c %d %d", &op, &a, &b)==3) {
+		Calc(op, a, b, reg);
+	}
+ 
+	return 0;
+} 
