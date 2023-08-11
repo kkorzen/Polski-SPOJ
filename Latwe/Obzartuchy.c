ef64ee7618afe17e1fc6f1060c314996c5b17589
kkorzen <141860845+kkorzen@users.noreply.github.com>
2023-08-11 11:06:08 +0200

    Create Obzartuchy.c

---
+++ Latwe/Obzartuchy.c
@@ -0,0 +1,43 @@
+#define _CRT_SECURE_NO_WARNINGS
+#define MAX_SEC 86400
+ 
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+ 
+int Calc(int* _t, int _N, int _M) {
+	int res = 0;
+	int* cookies_a_day = calloc(_N, sizeof(int));
+	int total_cookies = 0;
+ 
+	for (int i = 0;i < _N;i++) {
+		cookies_a_day[i] = MAX_SEC / _t[i];
+		total_cookies += cookies_a_day[i];
+	}
+ 
+	res = ceil((float)total_cookies/(float)_M);
+	
+	free(cookies_a_day);
+	return res;
+}
+ 
+int main() {
+	
+	int C;
+	scanf("%d", &C);
+ 
+	int N = 0, M = 0;
+	int* t;
+ 
+	for (int z = 0;z < C;z++) {
+		scanf("%d %d", &N, &M);
+		t = calloc(N, sizeof(int));
+		for (int v = 0;v < N;v++) {
+			scanf("%d", t+v);
+		}
+		printf("%d\n\n", Calc(t, N, M));
+		free(t);
+	}
+	
+	return 0;
+}
