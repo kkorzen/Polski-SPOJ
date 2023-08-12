ce87e2f0015eed8758ab9bea32f3d1c5a34ce994
kkorzen <141860845+kkorzen@users.noreply.github.com>
2023-08-12 19:43:52 +0200

    Create Samolot.c

---
+++ Latwe/Samolot.c
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+ 
+int calc(int _n1, int _k1, int _n2, int _k2)
+{
+	return _n1 * _k1 + _n2 * _k2;
+}
+
+int main() { 
+	int n1 = 0, k1 = 0, n2 = 0, k2 = 0;
+ 
+	scanf("%d %d %d %d", &n1, &k1, &n2, &k2);
+	printf("%d\n", calc(n1,k1,n2,k2));
+ 
+	return 0;
+} 
