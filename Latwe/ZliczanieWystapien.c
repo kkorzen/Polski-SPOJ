a219e19bc6e0a9d125327c72a745ebca37809bc3
kkorzen <141860845+kkorzen@users.noreply.github.com>
2023-08-12 19:44:55 +0200

    Create ZliczanieWystapien.c

---
+++ Latwe/ZliczanieWystapien.c
@@ -0,0 +1,29 @@
+#define _CRT_SECURE_NO_WARNINGS
+ 
+#include <stdio.h>
+#include <stdlib.h>
+ 
+int search(int n, int* arr, int l) {
+	int cnt = 0;
+	for (int i = 0;i < l;i++) {
+		if (arr[i] == n)
+			cnt++;
+	}
+	return cnt;
+}
+ 
+int main() {
+ 
+	int s = 0, l = 0;
+	int* arr;
+	while (scanf("%d %d", &s, &l) == 2) {
+		arr = calloc(l, sizeof(int));
+		for (int i = 0;i < l;i++) {
+			scanf("%d", arr + i);
+		}
+		printf("%d\n", search(s, arr, l));
+		free(arr);
+	}
+ 
+	return 0;
+} 
