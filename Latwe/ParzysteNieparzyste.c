500f7fe7529ca82615e82f49d2b318518c56800a
kkorzen <141860845+kkorzen@users.noreply.github.com>
2023-08-12 19:28:00 +0200

    Create ParzysteNieparzyste.c

---
+++ Latwe/ParzysteNieparzyste.c
@@ -0,0 +1,31 @@
+#define _CRT_SECURE_NO_WARNINGS
+ 
+#include <stdio.h>
+#include <stdlib.h>
+ 
+int main() {
+ 
+	int t, n;
+	int* arr;
+ 
+	scanf("%d", &t);
+ 
+	for (int i = 0;i < t;i++) {
+		scanf("%d", &n);
+		arr = calloc(n, sizeof(int));
+		for (int j = 0;j < n;j++) {
+			scanf("%d", &arr[j]);
+		}
+		for (int j = 1;j < n;j += 2) {
+			printf("%d ", arr[j]);
+		}
+		for (int j = 0;j < n;j += 2) {
+			printf("%d ", arr[j]);
+		}
+ 
+		puts("");
+		free(arr);
+	}
+ 
+	return 0;
+} 
