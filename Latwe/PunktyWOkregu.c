1e81e0f606e6b1fd14ed0cb4a64ee52a95ecbee4
kkorzen <141860845+kkorzen@users.noreply.github.com>
2023-08-12 19:51:02 +0200

    Create PunktyWOkregu.c

---
+++ Latwe/PunktyWOkregu.c
@@ -0,0 +1,34 @@
+#define _CRT_SECURE_NO_WARNINGS
+ 
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+ 
+typedef struct {
+	int xo, yo, r;
+}Circle;
+ 
+ 
+void Check(Circle c, int xp, int yp) {
+	double dist = sqrt(pow((c.xo - xp), 2) + pow((c.yo - yp), 2));
+	//printf("%f -- ", dist);
+	if (dist < (double)c.r) printf("I\n");
+	if (dist == (double)c.r) printf("E\n");
+	if (dist > (double)c.r) printf("O\n");
+}
+ 
+int main() {
+	Circle c;
+	int n;
+	int xp, yp;
+ 
+	scanf("%d %d %d", &c.xo, &c.yo, &c.r);
+	scanf("%d", &n);
+ 
+	for (int i = 0;i < n;i++) {
+		scanf("%d %d", &xp, &yp);
+		Check(c, xp, yp);
+	}
+ 
+	return 0;
+} 
