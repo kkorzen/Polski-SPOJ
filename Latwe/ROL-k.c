d525fb0e7cd66608b8b3767f91185765e25cffb7
kkorzen <141860845+kkorzen@users.noreply.github.com>
2023-08-12 19:33:20 +0200

    Create ROL-k.c

---
+++ Latwe/ROL-k.c
@@ -0,0 +1,38 @@
+#define _CRT_SECURE_NO_WARNINGS
+ 
+#include <stdio.h>
+#include <stdlib.h>
+ 
+void Shift_Left(int arr[], int len) {
+	int temp = arr[0];
+	for (int i = 1;i < len;i++) {
+		arr[i - 1] = arr[i];
+	}
+	arr[len - 1] = temp;
+}
+ 
+int main() {
+ 
+	int n, k;
+	int* tab;
+ 
+	scanf("%d %d", &n, &k);
+	tab = (int*)malloc(sizeof(int) * (n + 1));
+ 
+	for (int j = 0;j < n;j++) {
+		scanf("%d", tab + j);
+	}
+ 
+	for (int j = 0;j < k;j++) {
+		Shift_Left(tab, n);
+	}
+	
+	for (int j = 0;j < n;j++) {
+		printf("%d ", tab[j]);
+	}
+ 
+	free(tab);
+	puts("");
+ 
+	return 0;
+} 
