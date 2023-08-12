44a4c0f400bdfde4db026da1b4892fc003e903c0
kkorzen <141860845+kkorzen@users.noreply.github.com>
2023-08-12 19:49:04 +0200

    Create HarryAndBigDoughnuts.c

---
+++ Latwe/HarryAndBigDoughnuts.c
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS
+ 
+#include <stdio.h>
+ 
+int main() {
+	int c = 0;	// liczba kotow
+	int k = 0;	// udzwig Harry'ego
+	int w = 0;	// waga jednego paczka
+ 
+	int t = 0;
+	scanf("%d", &t);
+ 
+	for (int i = 0;i < t;i++) {
+		scanf("%d %d %d", &c, &k, &w);
+		if (c * w <= k) puts("yes");
+		else puts("no");
+	}
+ 
+	return 0;
+} 
