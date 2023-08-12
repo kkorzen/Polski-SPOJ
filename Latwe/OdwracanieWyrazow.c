486e35e23a3e20bebd0056d7eab353c1bafbc1fa
kkorzen <141860845+kkorzen@users.noreply.github.com>
2023-08-12 19:46:11 +0200

    Create OdwracanieWyrazow.c

---
+++ Latwe/OdwracanieWyrazow.c
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS
+ 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+ 
+char* ReverseStr(char* str) {
+	int len = strlen(str);
+	char* s = calloc(len + 1, sizeof(char));
+	for (int i = 0;i < len;i++)
+		s[i] = str[len - i - 1];
+	return s;
+}
+ 
+int main() {
+	char str[1001];
+ 
+	while (fgets(str, sizeof(str), stdin)) {
+		char* rstr = ReverseStr(str);
+		printf("%s\n", rstr);
+		free(rstr);
+	}
+ 
+	return 0;
+}
