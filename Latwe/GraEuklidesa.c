56288495602ac1b68fd48d9d909c328959d9288b
kkorzen <141860845+kkorzen@users.noreply.github.com>
2023-08-22 13:45:49 +0200

    Create GraEuklidesa.c

---
+++ Latwe/GraEuklidesa.c
@@ -0,0 +1,34 @@
+// Dyrektywa dla poprawnego dzialania funkcji scanf() w Visual Studio
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+int Calc(int _a, int _b) {
+	int a = _a, b = _b;
+	bool player = false;	// true -> first ; false -> second
+
+	while ((a != b) && (a > 0) && (b > 0)) {
+		player = (a < b) ? true : false;
+		if (player) {
+			b -= a;
+		}
+		else {
+			a -= b;
+		}
+	}
+	return a + b;
+}
+
+int main() {
+	int a, b, t;
+
+	scanf("%d", &t);
+	for (int i = 0;i < t;i++) {
+		scanf("%d %d", &a, &b);
+		printf("%d\n", Calc(a, b));
+	}
+
+	return 0;
+}
