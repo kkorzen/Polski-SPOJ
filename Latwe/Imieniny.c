39e0664985106828245857053601eacbcab4ee07
kkorzen <141860845+kkorzen@users.noreply.github.com>
2023-08-30 19:58:45 +0200

    Create Imieniny.c

---
+++ Latwe/Imieniny.c
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+char* Decide(int L, int C) {
+	if (L == 1) return "TAK";
+	if (L != 0) {
+		if (C % (L - 1) == 0) return "NIE";
+		else return "TAK";
+	}
+	return "NIE";
+}
+
+int main() {
+	int D;
+	unsigned long L, C;
+
+	scanf("%d", &D);
+
+	while (D--) {
+		scanf("%lu %lu", &L, &C);
+		printf("%s\n", Decide(L, C));
+	}
+
+	return 0;
+}
