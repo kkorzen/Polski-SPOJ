25e22e3453b33e5e61b79e452a904198c45e7652
kkorzen <141860845+kkorzen@users.noreply.github.com>
2023-09-01 14:32:17 +0200

    Create TagiHTML.c

---
+++ Latwe/TagiHTML.c
@@ -0,0 +1,33 @@
+// Dyrektywa dla poprawnego dzialania funkcji scanf() w Visual Studio
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+int main(){
+
+	char str[200];
+	_Bool bracket = 0; // 0 - closed | 1 - opened
+
+	while (fgets(str, 200, stdin)) {
+		int len = strlen(str);
+		
+		for (int i = 0;i < len;i++) {
+			if (str[i] == '<') {
+				bracket = 1;
+			}
+			if (bracket && str[i] != '>') {
+				str[i] = toupper(str[i]);
+			}
+			if (str[i] == '>') {
+				bracket = 0;
+			}
+		}
+
+		printf("%s\n", str);
+	}
+
+	return 0;
+}
