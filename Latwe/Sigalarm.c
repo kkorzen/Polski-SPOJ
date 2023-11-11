f335b04dd58be8842d812c8d31f0fb47809100e6
kkorzen <konradkorz61@gmail.com>
2023-11-12 00:11:48 +0100

    Create Sigalarm.c

---
+++ Latwe/Sigalarm.c
@@ -0,0 +1,23 @@
+#include <signal.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <unistd.h> // Tylko dla systemow UNIX (MacOS, Linux)
+
+volatile sig_atomic_t print_flag = false;
+
+void handle_alarm(int sig) {
+    print_flag = true;
+}
+
+int main() {
+    signal(SIGALRM, handle_alarm);
+    alarm(1);
+    for (;;) {
+        sleep(5);
+        if (print_flag) {
+            printf("Hello\n");
+            print_flag = false;
+            alarm(1);
+        }
+    }
+}
