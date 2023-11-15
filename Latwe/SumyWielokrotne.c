20a3ea07ec4666c203453d771aa15e1e2241a1c2
kkorzen <konradkorz61@gmail.com>
2023-11-15 19:23:10 +0100

    Create SumyWielokrotne.c

---
+++ Latwe/SumyWielokrotne.c
@@ -0,0 +1,29 @@
+/*
+    Kod zadania:    KC008
+    Link:           https://pl.spoj.com/problems/KC008/
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+
+    long long number = 0;
+    long long sum = 0;
+    long long total_sum = 0;
+
+    while (scanf("%lld", &number) == 1) {
+        if (number == 0) {
+            printf("%lld\n", sum);
+            total_sum += sum;
+            sum = 0;
+            continue;
+        }
+        sum += number;
+    }
+    printf("%lld", total_sum);
+
+
+    return 0;
+}
