bd86b6badf143bd31573a26b1738895976644316
kkorzen <konradkorz61@gmail.com>
2023-11-15 18:52:57 +0100

    Update ZliczanieLiczbIWyrazow.c

--- Latwe/ZliczanieLiczbIWyrazow.c
+++ Latwe/ZliczanieLiczbIWyrazow.c
@@ -4,28 +4,28 @@
 */
 
 #include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
 #include <ctype.h>
-
-#define MAX_STR_SIZE 1024
 
 int main() {
 
-    int numbers = 0;
-    int words = 0;
+    int numbers = 0, words = 0;
 
-    char str[MAX_STR_SIZE];
+    char* str_ptr = NULL;
+    long long str_max_len = 0;
+    long long str_len = 0;
 
-    while (fgets(str, MAX_STR_SIZE, stdin) != NULL) {
-        str[strcspn(str, "\n")] = 0;
-        int len = strlen(str);
-        for (int i = 0; i < len; i++) {
-            if (isalpha(str[i]) && (isspace(str[i + 1]) || str[i + 1] == '\0')) words++;
-            if (isdigit(str[i]) && (isspace(str[i + 1]) || str[i + 1] == '\0')) numbers++;
+
+    while (1) {
+        str_len = getline(&str_ptr, &str_max_len, stdin);
+        if (str_len == -1) break;
+        for (int i = 0; i < str_len; i++) {
+            if (isalpha(str_ptr[i]) && (isspace(str_ptr[i + 1]) || str_ptr[i + 1] == '\0')) words++;
+            if (isdigit(str_ptr[i]) && (isspace(str_ptr[i + 1]) || str_ptr[i + 1] == '\0')) numbers++;
         }
-        fflush(stdin);
         printf("%d %d\n", numbers, words);
         numbers = words = 0;
     }
 
+    return 0;
 }
