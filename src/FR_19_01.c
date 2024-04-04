c18b274e7997d53a26001112ffc746f31ac6fc6f
kkorzen <konradkorz61@gmail.com>
2024-04-04 12:25:27 +0200

    Add FR_19_01.c (IV edycja konkursu programistycznego)

---
+++ src/FR_19_01.c
@@ -0,0 +1,12 @@
+// Kod: FR_19_01
+// Link: https://pl.spoj.com/problems/FR_19_01/
+
+#include <stdio.h>
+
+int main()
+{
+    int a,b,c,d,e;
+    scanf("%d %d %d %d %d", &a, &b, &c, &d, &e);
+    printf("%d", a+b+c+d+e+5);
+    return 0;
+}
