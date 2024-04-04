f4080b5072636e14904f76f700af96e7a4105d32
kkorzen <konradkorz61@gmail.com>
2024-04-04 19:10:56 +0200

    Add FR_19_06.c (Raz, dwa, trzy)

---
+++ src/FR_19_06.c
@@ -0,0 +1,17 @@
+// Kod: FR_19_06
+// Link: https://pl.spoj.com/problems/FR_19_06/
+
+#include <stdio.h>
+
+int main()
+{
+    size_t a = 0;
+    scanf("%llu", &a);
+
+    if (a > 0)
+        puts("TAK");
+    else
+        puts("NIE");
+
+    return 0;
+}
