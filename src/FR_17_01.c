28af523fc5974845420ca3d531427baa939d73b5
kkorzen <konradkorz61@gmail.com>
2024-05-18 15:51:52 +0200

    Add FR_17_01.c

---
+++ src/FR_17_01.c
@@ -0,0 +1,11 @@
+// Kod: FR_17_01
+// Link: https://pl.spoj.com/problems/FR_17_01/
+
+#include <stdio.h>
+
+int main() {
+  short n = 0;
+  scanf("%hi", &n);
+  (n==2023)?puts("tak"):puts("nie");
+  return 0;
+}
