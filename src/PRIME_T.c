1b9b93fc14b05e3b5b9923ea5ba6bfe0457aa0ca
Konrad Korzeniowski <141860845+kkorzen@users.noreply.github.com>
2024-06-24 10:41:47 +0200

    Update PRIME_T.c

--- src/PRIME_T.c
+++ src/PRIME_T.c
@@ -1,39 +1,31 @@
-/*
-	Kod zadania: 	PRIME_T
-	Link:			https://pl.spoj.com/problems/PRIME_T/
-*/
+// Kod: PRIME_T
+// Link: https://pl.spoj.com/problems/PRIME_T/
 
 #include <stdio.h>
 #include <stdbool.h>
 
-bool Is_Prime(int n) {
-	char divisors = 0;
-
-	if (n == 1)
-		return false;
-
-	for (int i = 1; i <= n; i++) {
-		if (n % i == 0)
-			divisors++;
-		if (divisors > 2)
-			return false;
-	}
-
-	return true;
+bool IsPrime(int n) {
+    if (n < 2) return false;
+    if (n == 2) return true;
+    if (n % 2 == 0) return false;
+    for (int i = 3; (i * i) <= n; i += 2) {
+        if (n % i == 0) return false;
+    }
+    return true;
 }
 
 int main() {
 
-	int n, x;
-	scanf("%d", &n);
+    int n, x;
+    scanf("%d", &n);
 
-	for (int i = 0; i < n; i++) {
-		scanf("%d", &x);
-		if (Is_Prime(x))
-			puts("TAK");
-		else
-			puts("NIE");
-	}
+    for (int i = 0;i < n;i++) {
+        scanf("%d", &x);
+        if (IsPrime(x))
+            printf("TAK\n");
+        else
+            printf("NIE\n");
+    }
 
-	return 0;
+    return 0;
 }
