d0be9f50abe05127ed09f49401be1c8f4067f917
Konrad Korzeniowski <141860845+kkorzen@users.noreply.github.com>
2024-07-19 14:36:21 +0200

    Add FZI_STEFAN.c

---
+++ src/FZI_STEFAN.c
@@ -0,0 +1,24 @@
+// Kod: FZI_STEFAN
+// Link: https://pl.spoj.com/problems/FZI_STEF/
+
+#include <stdio.h>
+
+int main() {
+    int n = 0;
+    scanf("%d", &n);
+
+    int income = 0;
+    long long int sum = 0;
+    long long int max = 0;
+
+    while (n--) {
+        scanf("%d", &income);
+        sum += income;
+        if (sum > max) max = sum;
+        if (sum < 0) sum = 0;
+    }
+
+    printf("%llu\n", max);
+
+    return 0;
+}
