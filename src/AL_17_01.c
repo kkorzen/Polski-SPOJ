47564a36e9e277c0f15a7bbe124c530974df01f2
Konrad Korzeniowski <konradkorz61@gmail.com>
2024-10-22 15:22:15 +0200

    Add AL_17_01.c

---
+++ src/AL_17_01.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+  
+	printf("Mundial %c%c%c%c", 
+                'S'-'!',
+								'P'-' ',
+								'Q'-' ',
+								'R'-'('+'\n');
+  
+	return (int)('A'-'A');
+}
